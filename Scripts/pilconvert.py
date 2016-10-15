a1734191dfd744b4c1ec03dcce9163d51b1d5a0c
haggarezek <Ezekiel@ourbar.co.nz>
2016-10-15 21:39:22 +1300

    2nd Commit

---
+++ Scripts/pilconvert.py
@@ -0,0 +1,4 @@
+#!"c:\users\ezekiel\desktop\dev 1.9\1.9\scripts\python.exe"
+# EASY-INSTALL-SCRIPT: 'Pillow==3.4.1','pilconvert.py'
+__requires__ = 'Pillow==3.4.1'
+__import__('pkg_resources').run_script('Pillow==3.4.1', 'pilconvert.py')
